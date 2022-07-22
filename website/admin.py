ab009f30db7be9edea49e00454eab6e74f1cc1bd
Devin Mork <devin.mork@gmail.com>
2022-07-21 21:22:16 -0500

    Make post date-related field read-only in admin view.

--- website/admin.py
+++ website/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from website.models import Post, Page, UserProfile
 
 class PostAdmin(admin.ModelAdmin):
-  readonly_fields = ["slug",]
+  readonly_fields = ["slug", "date_created", "date_modified"]
 
 admin.site.register(UserProfile)
 

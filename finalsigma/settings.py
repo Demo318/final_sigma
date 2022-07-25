9aee4a845d117af9bc4af581c03d8328a82bcf0f
Devin Mork <devin.mork@gmail.com>
2022-07-24 20:55:31 -0500

    Trying heroku again.

--- finalsigma/settings.py
+++ finalsigma/settings.py
@@ -173,6 +173,7 @@ MESSAGE_TAGS = {
 # GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
 #     BASE_DIR / 'final-sigma-website-bucket-credentials.json'
 # )
+
 if config('CODE_ENVIRONMENT') == 'production':
     DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = config('GOOGLE_BUCKET_NAME')

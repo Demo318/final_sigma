0a53da49d11c05da2864af4576684409fb030bf0
Devin Mork <devin.mork@gmail.com>
2022-07-25 14:34:26 -0500

    Remove tinymce in urls.py

--- finalsigma/urls.py
+++ finalsigma/urls.py
@@ -21,5 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
-    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

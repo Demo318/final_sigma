c144be36af07c3bfaf1c14f811ab1fa9a6a432d5
Devin Mork <devin.mork@gmail.com>
2022-07-24 20:42:52 -0500

    Configure static and media file roots and urls.

--- finalsigma/settings.py
+++ finalsigma/settings.py
@@ -125,6 +125,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Configure Media Directory
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
